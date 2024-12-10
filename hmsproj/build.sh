0b68526f8977ba2572117a176ba040a1e8d7771c
Immanual <emmanuel.shalu@gmail.com>
2024-12-11 01:30:29 +0530

    change in build.sh

--- hmsproj/build.sh
+++ hmsproj/build.sh
@@ -5,5 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate --noinput
+python manage.py makemigrations hmsapp
+python manage.py migrate hmsapp
+python manage.py migrate
